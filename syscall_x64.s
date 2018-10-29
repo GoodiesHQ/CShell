be6296d92b6a8e8d501cbdc03a48acfe5f746e48
GoodiesHQ <goodies@protonmail.com>
2018-10-29 09:22:09 -0700

    Fixed syscall wrapper to properly handle syscalls with 6 parameters

--- syscall_x64.s
+++ syscall_x64.s
@@ -8,12 +8,24 @@ cshell3:
 cshell4:
 cshell5:
 cshell6:
-	mov rax,rdi
-	mov rdi,rsi
-	mov rsi,rdx
-	mov rdx,rcx
-	mov r10,r8
-	mov r8,r9
-	mov r9, [rsp + 0x10]
+	//; create a stack frame
+	push rbp
+	mov rbp, rsp
+
+	//; move all parameters to set up a syscall
+	mov rax, rdi
+	mov rdi, rsi
+	mov rsi, rdx
+	mov rdx, rcx
+	mov r10, r8
+	mov r8,  r9
+	mov r9, [rbp + 0x10]
+
+	//; execute the syscall
 	syscall
+
+	//; destroy the stack frame
+	mov rsp, rbp
+	pop rbp
+
 	ret
--- syscall_x86.s
+++ syscall_x86.s
@@ -8,17 +8,26 @@ cshell3:
 cshell4:
 cshell5:
 cshell6:
+	//; create a stack frame to preserve previous base pointer
 	push ebp
 	mov ebp, esp
+
+	//; move all possible arguments into gp registers
 	mov eax, [ebp+0x08]
 	mov ebx, [ebp+0x0c]
 	mov ecx, [ebp+0x10]
 	mov edx, [ebp+0x14]
 	mov esi, [ebp+0x18]
 	mov edi, [ebp+0x1c]
+
+	//; save current ebp
 	push ebp
 	mov ebp, [ebp+0x20]
+
+	//; kernel int
 	int 0x80
+
+	//; restore ebp and destroy the stack frame
 	pop ebp
 	mov esp, ebp
 	pop ebp
